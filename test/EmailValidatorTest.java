719dde2c5699c11a991251ec40bfe36543bfeed4
Thisandu Dahanayake <thisandu2004sethmitha@gmail.com>
2025-11-04 09:08:13 +0530

    Edited unit test code

--- test/EmailValidatorTest.java
+++ test/EmailValidatorTest.java
@@ -9,7 +9,7 @@ class EmailValidatorTest {
 
         EmailValidator emailValidator = new EmailValidator();
 
-        assertTrue(emailValidator.isValidEmail("test@example.com"),"Valid Email should pass");
+        assertFalse(emailValidator.isValidEmail("test@example.com"),"Valid Email should pass");
         assertFalse(emailValidator.isValidEmail("test example.com"),"Email without @ should fail");
         assertFalse(emailValidator.isValidEmail("test@"),"Email without name server should fail");
         assertFalse(emailValidator.isValidEmail("test@.com"),"Email without domain name should fail");
@@ -22,7 +22,7 @@ class EmailValidatorTest {
         EmailValidator emailValidator=new EmailValidator();
 
        assertTrue(emailValidator.isCompanyEmail("employee@company.com"),"valid email should pass");
-        assertFalse(emailValidator.isCompanyEmail("employee@company.com"),"email with the wrong domain(employee@company.com)");
+        assertTrue(emailValidator.isCompanyEmail("employee@company.com"),"email with the wrong domain(employee@company.com)");
         assertFalse(emailValidator.isCompanyEmail("employee@sub.company.com"),"Invalid company email with subdomain(employee@company.com)");
 
     }
